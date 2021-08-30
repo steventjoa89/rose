33d62ee356e073dad97b50e5e2b10e91a4216b69
Steven Tjoa <steventjoa89@gmail.com>
2021-08-31 02:53:08 +0700

    Done for home page.

--- php/subscribe.php
+++ php/subscribe.php
@@ -1,13 +1,26 @@
 <?php
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-  // collect value of input field
-  $name = $_POST['fname'];
-  if (empty($name)) {
-    echo "Name is empty";
-  } else {
-    echo $name;
+  $data = [];
+
+  if(isset($_POST['email'])){
+    // Insert into database
+    $servername = "localhost";
+    $username = "admin";
+    $password = "admin";
+    $dbname = "rose";
+
+    $conn = new mysqli($servername, $username, $password, $dbname);
+    if ($conn->connect_error) {
+      die("Connection failed: " . $conn->connect_error);
+    }
+    $sql = "INSERT INTO subscriber (email_address, sent_subscription_email, created_at, updated_at) VALUES ('".$_POST['email']."', 0, now(), now())";
+    if ($conn->query($sql) === TRUE) {
+      echo "OK";
+    } else {
+      echo "Error: " . $sql . "<br>" . $conn->error;
+    }
+
+    $conn->close();
   }
-}
 
-
+  
 ?>
